a76ee14ecae367bb6be13304b1761f39a44b2e20
neslihanaydin <neslihanaydin43@gmail.com>
2018-03-04 22:55:20 +0300

    Spatial type ekleme
    Geography turunce spatial type veriler eklendi

---
+++ Odev-2/SecmenEkleme.sql
@@ -0,0 +1,16 @@
+
+
+insert into Mahalle_Koy VALUES (
+	'Bogazkent Mah',
+	 geography::STGeomFromText('POLYGON((-422.358 47.653 , -422.348 47.649, -422.348 47.658, -422.358 47.658, -422.358 47.653))', 4326)
+);
+
+insert into Secmen VALUES (
+	122345511,
+	 'Sema', 
+	 'AYDIN',
+	 geography::STGeomFromText('POLYGON((-122.358 47.653 , -122.348 47.649, -122.348 47.958, -122.358 47.958, -122.358 47.653))', 4326)
+);
+
+
+
